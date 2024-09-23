b4330c10ccccb198f718b98e57ca990024539b69
linhareshian <hian.linhares@rethink.dev>
2024-09-22 22:59:27 -0300

    primeiros aprendizados de react

---
+++ HL_React/src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import { Button} from './Post'
+
+
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+   <h1>hello World</h1>
+  )
+}
+
+export default App
+
+
+//JSX = Javascript + XML
+
---
+++ HL_React/src/Post.jsx
@@ -0,0 +1,20 @@
+import Post from './Post'
+
+
+function Post(){
+    return <p>Post</p>
+}
+
+export default Post
+
+
+export function Button(){
+    return (
+        <p>button</p>
+    )
+}
+
+
+
+// default exports vs named exports
+
